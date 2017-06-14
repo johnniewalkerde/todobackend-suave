336c7414e24f1fc916b758d4daeb656bf152dffd
Johannes Körner <koerner.j@st-sportservice.com>
2017-06-14 13:55:32 +0200

    POST dummy

--- app.fsx
+++ app.fsx
@@ -89,6 +89,7 @@ let app =
         + "<br/>Expose Headers:" + corsConfig.exposeHeaders.ToString()
         + "<br/>Methods:" + corsConfig.allowedMethods.ToString()
         )
+      POST >=> cors corsConfig >=> plainTextMime >=> OK "POST OK"
     ]
     
 #if DO_NOT_START_SERVER
